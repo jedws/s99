9d2ee9aef008850a719641afa14bb2ae192f57d6
Jed Wesley-Smith <jed@atlassian.com>
2015-11-30 09:09:23 +1100

    use implicit class instead

--- src/main/scala/s99/ArithmeticSolutions.scala
+++ src/main/scala/s99/ArithmeticSolutions.scala
@@ -1,12 +1,9 @@
 package s99
 
-trait ArithmeticSolutions {
+object ArithmeticSolutions {
 
   // add new functions to integers
-  implicit def extendInt(n: Int): ExtendedInt = ExtendedInt(n)
-
-  case class ExtendedInt(n: Int) {
-
+  implicit class ExtendedInt(n: Int) {
     def isPrime: Boolean = ???
     def isCoprimeTo(n: Int): Boolean = ???
     def totient: Int = ???
@@ -16,7 +13,6 @@ trait ArithmeticSolutions {
     def primeFactorMultiplicityMap: Map[Int, Int] = ???
     def listPrimesinRange(r: Range): List[Int] = ???
     def goldbach: (Int, Int) = ???
-
   }
 
   def primes: Stream[Int] = ???
@@ -24,5 +20,4 @@ trait ArithmeticSolutions {
   def listPrimesinRange(r: Range): List[Int] = ???
   def printGoldbachList(r: Range): List[String] = ???
   def printGoldbachListLimited(r: Range, limit: Int): List[String] = ???
-
 }
